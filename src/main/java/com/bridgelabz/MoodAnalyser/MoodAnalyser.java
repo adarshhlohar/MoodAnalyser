3b6c502df4618ae48df774350d0cf3d96d8511d2
adarshhlohar <adarshhlohar@gmail.com>
2023-12-04 18:04:16 +0530

    first commit

---
+++ src/main/java/com/bridgelabz/MoodAnalyser/MoodAnalyser.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.MoodAnalyser;
+
+public class MoodAnalyser {
+
+	public String analyseMood(String mood) {
+		return mood;
+	}
+}
---
+++ src/test/java/com/bridgelabz/MoodAnalyser/MoodAnalyserTest.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.MoodAnalyser;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+	
+	MoodAnalyser moodAnalyser;
+	
+	@Before
+	public void setUp() {
+		moodAnalyser = new MoodAnalyser();
+	}
+	
+	@Test
+	public void analyseMood() {
+		String mood = moodAnalyser.analyseMood("Happy");
+		assertEquals("Happy", mood);
+	}
+}
