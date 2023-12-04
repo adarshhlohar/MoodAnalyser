c28db99c9d2be62c0c569924e5630d57a5ee6b40
adarshhlohar <adarshhlohar@gmail.com>
2023-12-04 22:48:31 +0530

    Given I am in Any Mood message Should Return HAPPY

--- src/main/java/com/bridgelabz/MoodAnalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser/MoodAnalyser.java
@@ -3,11 +3,11 @@ package com.bridgelabz.MoodAnalyser;
 public class MoodAnalyser {
 
 	public String analyseMood(String mood) {
-		if(mood == "I am in Sad Mood") {
+		if (mood == "I am in Sad Mood") {
 			return "Sad";
+		} else {
+			return "Happy";
 		}
-		return mood;
 	}
 
 }
-
